8a3c52179eb5296d3ac49569a852a852372693d8
QuyetTran1112 <133133145+QuyetTran1112@users.noreply.github.com>
2024-05-21 21:33:20 +0700

    Update SeekBar Custom

--- app/src/main/java/com/example/quackrunner/Activity/QuackRunnerActivity.java
+++ app/src/main/java/com/example/quackrunner/Activity/QuackRunnerActivity.java
@@ -1,6 +1,12 @@
 package com.example.quackrunner.Activity;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,5 +35,21 @@ public class QuackRunnerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        SeekBar seekBar = binding.customSeekbar;
+        LayerDrawable layerDrawable = (LayerDrawable) seekBar.getProgressDrawable();
+
+        if (layerDrawable != null) {
+            // Dùng ID của item hoặc thứ tự
+            Drawable waveDrawable = layerDrawable.getDrawable(1); // Lấy drawable thứ 2 từ LayerDrawable
+            if (waveDrawable instanceof AnimationDrawable) {
+                AnimationDrawable animationDrawable = (AnimationDrawable) waveDrawable;
+                animationDrawable.start();
+            }
+        } else {
+            Toast.makeText(this, "LayerDrawable is null", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
