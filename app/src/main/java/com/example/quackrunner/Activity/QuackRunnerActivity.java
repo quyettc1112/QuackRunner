84a570c71325b63aa9a6d1b75b565e6a00003c8b
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-21 22:52:56 +0700

    Update Logic Quack

--- app/src/main/java/com/example/quackrunner/Activity/QuackRunnerActivity.java
+++ app/src/main/java/com/example/quackrunner/Activity/QuackRunnerActivity.java
@@ -1,5 +1,8 @@
 package com.example.quackrunner.Activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -14,6 +17,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.example.quackrunner.R;
 import com.example.quackrunner.databinding.ActivityQuackRunnerBinding;
 
@@ -21,7 +26,7 @@ public class QuackRunnerActivity extends AppCompatActivity {
 
     // Khai báo bindingđể thay thế findViewbyId(),
     private ActivityQuackRunnerBinding binding;
-
+    private LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +41,23 @@ public class QuackRunnerActivity extends AppCompatActivity {
             return insets;
         });
 
-        SeekBar seekBar = binding.customSeekbar;
+        SeekBar seekBarLine1 = binding.customSeekbarLine1;
+        SeekBar seekBarLine2 = binding.customSeekbarLine2;
+        SeekBar seekBarLine3 = binding.customSeekbarLine3;
+        SeekBar seekBarLine4 = binding.customSeekbarLine4;
+
+        setupSeekBarAnimation(seekBarLine1, 200);
+        setupSeekBarAnimation(seekBarLine2, 300);
+        setupSeekBarAnimation(seekBarLine3, 200);
+        setupSeekBarAnimation(seekBarLine4, 300);
+
+
+    }
+
+    private void setupSeekBarAnimation(SeekBar seekBar, long shakeDuration) {
         LayerDrawable layerDrawable = (LayerDrawable) seekBar.getProgressDrawable();
 
         if (layerDrawable != null) {
-            // Dùng ID của item hoặc thứ tự
             Drawable waveDrawable = layerDrawable.getDrawable(1); // Lấy drawable thứ 2 từ LayerDrawable
             if (waveDrawable instanceof AnimationDrawable) {
                 AnimationDrawable animationDrawable = (AnimationDrawable) waveDrawable;
@@ -50,6 +67,15 @@ public class QuackRunnerActivity extends AppCompatActivity {
             Toast.makeText(this, "LayerDrawable is null", Toast.LENGTH_LONG).show();
         }
 
+        ObjectAnimator shakeAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                seekBar,
+                PropertyValuesHolder.ofFloat("rotation", -1.15f, 1.15f)
+        );
+        shakeAnimator.setDuration(shakeDuration);
+        shakeAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        shakeAnimator.setRepeatMode(ValueAnimator.REVERSE);
 
+        // Bắt đầu animation
+        shakeAnimator.start();
     }
 }
