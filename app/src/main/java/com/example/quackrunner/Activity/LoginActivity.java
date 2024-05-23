8498b7a7207e63874901d3b52dbd1fd1d9c51052
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-23 23:22:33 +0700

    Update UI

--- app/src/main/java/com/example/quackrunner/Activity/LoginActivity.java
+++ app/src/main/java/com/example/quackrunner/Activity/LoginActivity.java
@@ -53,38 +53,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 boolean isAuthenticated = authenticateUser(userList,username.getText().toString().trim(),password.getText().toString().trim());
                 if (isAuthenticated){
+                    Intent it = new Intent(LoginActivity.this, StartedActivity.class);
                     Toast.makeText(LoginActivity.this,"Login successfully",Toast.LENGTH_SHORT).show();
+                    startActivity(it);
+                    finish();
                 }else {
                     Toast.makeText(LoginActivity.this,"Login fail",Toast.LENGTH_SHORT).show();
                 }
             }
         });
-//        EdgeToEdge.enable(this);
-//
-//        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//
-//        intentToStartedActivity();
-
     }
     public static boolean authenticateUser(List<User> userList, String username, String password) {
         return userList.stream()
                 .anyMatch(user -> user.getUsername().equals(username) && user.getPassword().equals(password));
     }
-//    private void intentToStartedActivity () {
-//        Intent it = new Intent(this, StartedActivity.class);
-//        binding.bntIntentStarted.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(it);
-//                finish();
-//            }
-//        });
-//
-//    }
+
 }
--- app/src/main/java/com/example/quackrunner/Activity/StartedActivity.java
+++ app/src/main/java/com/example/quackrunner/Activity/StartedActivity.java
@@ -23,10 +23,6 @@ public class StartedActivity extends AppCompatActivity {
 
         binding = ActivityStartedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
