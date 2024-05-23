70fece4afcd8264fdc691aea8066b29c0db3afbb
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-05-24 02:54:08 +0700

    Update Tutorial

--- app/src/main/java/com/example/quackrunner/Activity/StartedActivity.java
+++ app/src/main/java/com/example/quackrunner/Activity/StartedActivity.java
@@ -1,6 +1,8 @@
 package com.example.quackrunner.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.quackrunner.Adapter.IntroSliderAdapter;
+import com.example.quackrunner.Model.TutorialSlider;
 import com.example.quackrunner.R;
 import com.example.quackrunner.databinding.ActivityStartedBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StartedActivity extends AppCompatActivity {
 
@@ -24,5 +31,33 @@ public class StartedActivity extends AppCompatActivity {
         binding = ActivityStartedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+
+
+        Intent play = new Intent(StartedActivity.this, QuackRunnerActivity.class);
+        Intent tutorial = new Intent(StartedActivity.this, TutorialActivity.class);
+        binding.play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(play);
+            }
+        });
+        binding.tutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(tutorial);
+            }
+        });
+        binding.setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
+
 }
